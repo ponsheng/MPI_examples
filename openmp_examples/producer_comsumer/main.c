f4122cc52ee18512a647f3b102c51868eef02d1d
ponsheng <a7061120@gmail.com>
2016-12-20 20:34:11 +0800

    producer_comsumer full function with memory leak

--- openmp_examples/producer_comsumer/main.c
+++ openmp_examples/producer_comsumer/main.c
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
 #ifdef _OPENMP
 #include <omp.h>
@@ -21,13 +22,15 @@ typedef struct entry
 } entry;
 
 char** keywords = NULL;
+int* keyword_table = NULL;
+
 int keyword_count =0;
 
 entry* list_head = NULL;
 entry* list_end = NULL;
 
-entry* all_head = NULL;
-int first = 0;
+//entry* all_head = NULL;
+//int first = 0;
 
 void examine (char* examine_string);
 int enqueue(char* string, int read);
@@ -44,27 +47,22 @@ int main(){
 
 	if(errno)
 		printf("opendir error: %d\n",errno);
-
-
 		
 	readkeyword(KEYWORD_FILE);
 
-
-
 	#pragma omp parallel num_threads(NUM)
 	{
-	struct dirent* dirent;
 	if(omp_get_thread_num() < NUM/2)
 	{
+		struct dirent* dirent;
 		FILE * fp;
 		char filename[30] = "\0" ;
 		char * line = NULL;
 		size_t len = 0;
 		ssize_t read;
-
-		
+	
 		while(1)
-		{			
+		{
 			#pragma omp critical(readdir)
 			{
 				dirent = readdir(dir_stream);
@@ -77,7 +75,7 @@ int main(){
 				strcat(filename,DIR_NAME);
                 strcat(filename,"/");
                 strcat(filename,dirent->d_name);
-				printf("proc %d read  %s \n",omp_get_thread_num(),filename);
+				printf("proc %d read  |%s|\n",omp_get_thread_num(),filename);
 				
 				fp = fopen(filename, "r");
 				if (fp == NULL)
@@ -89,22 +87,24 @@ int main(){
 				//getline and send to queue	
 				while ((read = getline(&line, &len, fp)) != -1)
 				{
-					printf("read %zu \n", read);
+					//printf("read %zu \n", read);
 					//printf("%s", line);
 					enqueue(line,read);
 				}
 				//if(line)
-					//free(line);
-				//fclose(fp);
+				//	free(line);
+				fclose(fp);
 			}
 		}
-		producer_count ++;
 		printf("%d finish\n",omp_get_thread_num());
 
-		if(producer_count == NUM/2)
+		int _count = __sync_fetch_and_add(&producer_count,1);
+		printf("producer count: %d %d\n",_count,NUM/2);
+
+		if( _count == NUM/2 -1)
 		{
 			//printf("%d last\n",omp_get_thread_num());
-			printf("all finish\n");
+			printf("all producers finish\n");
 			stop = 1;
 			if(closedir(dir_stream))
 				printf("closedir error: %d",errno);
@@ -114,11 +114,12 @@ int main(){
 	//consumer
 	else if(omp_get_thread_num() >= NUM/2)
 	{
-		goto end;
+		//goto end;
 		//fgetc(stdin);
 		char* string;
 		char* token;
 		char* save_ptr;
+
 		while( list_head||!stop)
 		{
 			if(!dequeue(&string))
@@ -126,6 +127,7 @@ int main(){
 				//printf("%d get %s",omp_get_thread_num(),string);
 				//printf("%d get \n",omp_get_thread_num());
 				//process string
+	
 				token = strtok_r(string," ",&save_ptr);
 
 				while(token != NULL)
@@ -137,19 +139,24 @@ int main(){
 
 				//process end			
 	
-				//free(string);
+				free(string);
 			}
 		}
-	end:
+		end:
 		printf("%d finish\n",omp_get_thread_num());	
 	}
 
 	}//omp finish
 
+	for(int i =0; i<keyword_count; i++)
+	{
+		printf("%s: %d\n",keywords[i],keyword_table[i]);
+	}
 	
 	clean_list();
-	//free(keywords[0]);
-	//free(keywords);
+	free(keywords[0]);
+	free(keywords);
+	free(keyword_table);
 	return 0;
 }
 
@@ -157,11 +164,11 @@ int enqueue(char* string, int read)
 {
 	struct entry* node_ptr;
 	node_ptr = (struct entry*) malloc (sizeof(struct entry));
-	node_ptr->string = (char*)malloc(sizeof(char)*(read));
+	node_ptr->string = (char*)malloc(sizeof(char)*(read+1));
 	strcpy(node_ptr->string, string);
 	node_ptr->len = read;
 	node_ptr->next = NULL;
-	#pragma omp critical
+	#pragma omp critical(list)
 	{
 		//printf("len: %d \n", node_ptr->len);
 		if(list_head)
@@ -170,11 +177,6 @@ int enqueue(char* string, int read)
 			list_head = node_ptr;
 
 		list_end = node_ptr;
-		if(first ==0)
-		{
-			first++;
-			all_head = node_ptr;
-		}
 	}	
 		//printf("len: %d \n", read);
 		//printf("%s", string);
@@ -187,12 +189,12 @@ int dequeue(char** string)
 		return -1;
 	
 	entry* temp;
-	#pragma omp critical
+	#pragma omp critical(list)
 	{
 		*string = list_head->string;
 		temp = list_head;
 		list_head = list_head->next;
-		//free(temp);
+		free(temp);
 	}
 
 	return 0;
@@ -205,10 +207,10 @@ void clean_list()
 	while(list_head)
 	{
 		c++;
-		//free(list_head->string);
+		free(list_head->string);
 		temp = list_head;
 		list_head = list_head->next;	
-		//free(temp);
+		free(temp);
 		//printf("clean one");
 	}
 	printf("clean %d entry\n",c);
@@ -225,15 +227,19 @@ void readkeyword()
 	char *token;
 	int size = 0;
 
+	 
+
 	keyword_fp = fopen(KEYWORD_FILE, "r");
 	keyword_read = getline(&get_line, &keyword_len, keyword_fp);
 	fclose(keyword_fp);	
 
 	keyword_line = strdup(get_line);
 
+	
+
 	//count size
 	token = strtok(get_line," ");
-	while( token != NULL ) 
+	while( token != NULL && *token != '\n' ) 
 	{
 		size++;
 		//printf( " %s\n", token);
@@ -244,27 +250,32 @@ void readkeyword()
 
 	//fill into global keywords
 	keywords = (char **) malloc(sizeof(char*)*size);
+	keyword_table = (int*) malloc(sizeof(int) * size);
 	
+	memset(keyword_table, 0, size*sizeof(int));
+
 	keywords[0] =  strtok(keyword_line," ");
 	printf("%s\n",keywords[0]);
 	for(int i =1; i<size; i++)
 	{
 		keywords[i] = strtok(NULL, " ");
-		printf("%s\n",keywords[i]);
+		printf("%s: %d\n",keywords[i],keyword_table[i]);
 	}
 	
-	//free(get_line);
-
+	free(get_line);
 }
 
 void examine(char* string)
 {
 	for(int i=0; i<keyword_count;i++)
 	{
+		char* p = string;
+		for ( ; *p; ++p) *p = tolower(*p);
 		//printf("%s com to %s\n",string,keywords[i]);
 		if(! strcmp(string,keywords[i]))
 		{
-			printf("%d get %s\n",omp_get_thread_num(),string);
+			//printf("%d get %s\n",omp_get_thread_num(),string);
+			__sync_fetch_and_add(&(keyword_table[i]),1);
 		}
 	}
 }
